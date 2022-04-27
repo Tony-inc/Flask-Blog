78933e07bab49b64462eecf4a7e46d7b27a87598
antonyusupov <yusupov.anton2001@yandex.com>
2022-04-27 13:18:51 +0300

    Complete Blog

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+# Enable Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 
