de84fae3950954ca105b3b7fbeb3a0f367d42865
antonyusupov <yusupov.anton2001@yandex.com>
2022-04-28 17:26:43 +0300

    Complete Blog and Contact Module

--- main.py
+++ main.py
@@ -40,7 +40,11 @@ def admin_only(f):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///blog.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
